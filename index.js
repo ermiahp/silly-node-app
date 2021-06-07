4cf713d076464110904255da18ed770e496db2b4
ermiahp <ermiahasanpour@gmail.com>
2021-06-07 09:51:20 +0200

    Add Breed selection feature
    Fix second TODO item,
    
    I added a new function that gets the Breed name and it will return a random picture of a dog in that breed.

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const agent = require('superagent');
 const asciify = require('asciify-image');
 
 const DOG_API_URL = 'https://dog.ceo/api/breeds/image/random';
+const DOG_WITH_BREED_API_URL = (breed) => 'https://dog.ceo/api/breed/' + breed + '/images/random';
 
 const getDogImageURL = async () => {
-  const { body: { message: dogUrl }} = await agent.get(DOG_API_URL);
+  const { body: { message: dogUrl } } = await agent.get(DOG_API_URL);
+  return dogUrl;
+};
+
+const getDogWithBreedImageURL = async (breed) => {
+  const { body: { message: dogUrl } } = await agent.get(DOG_WITH_BREED_API_URL(breed));
   return dogUrl;
 };
 
 (async () => {
   // Get a URL for a random image of a dog
-  const dogUrl = await getDogImageURL();
-
+  const dogUrl = await getDogWithBreedImageURL('hound');
+  // const dogUrl = await getDogImageURL();
   // Download the image
   const { body: data } = await agent.get(dogUrl);
 
